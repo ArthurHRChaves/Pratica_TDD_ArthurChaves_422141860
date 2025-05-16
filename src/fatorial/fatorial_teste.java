67c9d110bec214b0512aed38093644b9213226af
Arthur Henrique Ribeiro Chaves <ahrchaves19@gmail.com>
2025-05-15 22:46:47 -0300

    Teste inicial calculadora fatorial

---
+++ src/fatorial/fatorial_teste.java
@@ -0,0 +1,14 @@
+package fatorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class fatorial_teste {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
